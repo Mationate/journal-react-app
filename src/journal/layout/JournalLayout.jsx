b8cc416aac549fdd1e49a9a3b82e44df505a0d1e
Matias Onate <mationate.0@gmail.com>
2022-12-27 15:03:15 -0300

    journal app front

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,18 +1,29 @@
-import { Box } from "@mui/system"
-import { Navbar } from "../components/";
-const drawerWidth = 240;
-export const JournalLayout = ({children}) => {
+
+
+import { Toolbar } from '@mui/material';
+import { Box } from '@mui/system'
+import { Navbar, SideBar } from '../components';
+
+
+const drawerWidth = 280;
+
+export const JournalLayout = ({ children }) => {
   return (
-        <Box sx={{display: 'flex'}}>
-            {/* navbar drawerwidth*/}
-            <Navbar/>
-            {/* sidebar drawerwidth*/}
-            <Box component='main'
-                sx={{flexGrow:1,p:3}}
-                >
-                    {/* toolbar */}
-                    {children}
-            </Box>
+    <Box sx={{ display: 'flex' }}>
+
+        <Navbar drawerWidth={ drawerWidth } />
+
+        <SideBar drawerWidth={ drawerWidth } />
+
+        <Box 
+            component='main'
+            sx={{ flexGrow: 1, p: 3 }}
+        >
+            <Toolbar />
+
+            { children }
+            
         </Box>
-    )
+    </Box>
+  )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,29 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { AddOutlined, MailOutline } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView } from "../views/NoteView"
+import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography variant=''>Veniam excepteur excepteur dolore consequat eu non culpa dolore dolore ullamco cupidatat velit Lorem eiusmod. Proident fugiat est aliqua enim pariatur laborum quis proident quis est laborum magna. Esse adipisicing qui tempor dolor anim labore fugiat adipisicing ea.</Typography>
+      {/* <Typography variant=''>Veniam excepteur excepteur dolore consequat eu non culpa dolore dolore ullamco cupidatat velit Lorem eiusmod. Proident fugiat est aliqua enim pariatur laborum quis proident quis est laborum magna. Esse adipisicing qui tempor dolor anim labore fugiat adipisicing ea.</Typography> */}
       {/* <MailOutline/> */}
+      <NothingSelectedView/>
+      {/* <NoteView/> */}
+
+      <IconButton
+      size='large'
+      sx={{
+        color:'white',
+        backgroundColor:'error.main',
+        ':hover': {backgroundColor:'error.main', opacity:0.9},
+        position:'fixed',
+        right:50,
+        bottom:50
+      }}>
+        <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
